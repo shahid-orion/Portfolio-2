c544d348643761b2a20c662b412481f1ebc1a139
Mohammad Islam <sroutfit@gmail.com>
2023-12-28 07:07:54 +1100

    adding dropbox project

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -21,7 +21,7 @@ const Skills = ({ skills }: Props) => {
         Skills
       </h3>
       <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm">
-        Hover over a skill for current proficiency
+        Hover over any skill for current proficiency
       </h3>
 
       <div className="grid grid-cols-4 md:grid-cols-6 gap-5 h-80 mt-10 md:mt-0">
